cf8f10614a5247a0d3f007a3ee73fe18a4efa9f9
Prasad <prasad@pschaudhari1712.com>
2024-01-31 11:11:14 +0530

    added gitignore

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product")
 
 const getAllProducts = async(req,res)=>{
-    const {company,name,featured,sort} = req.query
+    const {company,name,featured,sort,select} = req.query
     console.log(req.query);
     const queryObj = {}
     if(company){
@@ -17,16 +17,31 @@ const getAllProducts = async(req,res)=>{
     let apidata = Product.find(queryObj)
 
     if(sort){
-        let sortFix = sort.replace(","," ")
+        let sortFix = sort.split(",").join(" ")
         apidata = apidata.sort(sortFix)
+        // queryObj.sort = sortFix
     }
+    if(select){
+        // let selectFix = select.replace(","," ")  --> gives incorrect when three op 
+        let selectFix = select.split(",").join(" ")
+        apidata = apidata.select(selectFix)
+        // queryObj.sort = sortFix
+    }
+
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+    let skip = (page-1)*limit
+    apidata = apidata.skip(skip).limit(limit)
+
     console.log(queryObj);
-    const myData = await apidata
-    res.status(200).json({myData})
+    // const myData = await Product.find(queryObj).sort(sort) --> not working
+    const Products = await apidata
+    res.status(200).json({Products ,nbHits:Products.length})
 }
 const getAllProductsTesting = async(req,res)=>{
-    const myData = await Product.find(req.query).sort("price")
-    res.status(200).json({myData})
+    // const myData = await Product.find(req.query).sort("-price")
+    const myData = await Product.find(req.query).skip(2)
+    res.status(200).json({myData , nbHits:myData.length})
 }
 
 module.exports = {getAllProducts,getAllProductsTesting}
